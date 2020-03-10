f883486afaa6bdd88d9d3924d08b5cc3f879cd3c
kaushikeronkan <kaushik.aparnathi@eronkan.com>
2020-03-10 17:16:25 +0530

    bug fixed in App

--- passport-config.js
+++ passport-config.js
@@ -1,32 +1,29 @@
-const LocalStrategy=require('passport-local').Strategy
-const bcrypt=require('bcrypt')
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
 
-function intialatize(passport,getUserByEmail){
-    const authenticateUser=async (email,password,done)=>{
-        const user=getUserByEmail(email)
-        if(user == null){
-            return done(null,false,{message:'No user with that email'})
-        }
-
-        try{
-            if(await bcrypt.compare(password,user.password)){
-                return done(null,user)
-            }else{
-                return done(null,false,{message:'Password is incorrect'})
-            }
-        }
-        catch(e){
-            console.log("fail"+e)
-            return done(e)
-        }
-
+function initialize(passport, getUserByEmail, getUserById) {
+  const authenticateUser = async (email, password, done) => {
+    const user = getUserByEmail(email)
+    if (user == null) {
+      return done(null, false, { message: 'No user with that email' })
     }
 
-    passport.use(new LocalStrategy({ usernameField: 'backendEmail' }, authenticateUser))
-    passport.serializeUser((user, done) => done(null, user.id))
-    passport.deserializeUser((id, done) => {
-      return done(null, getUserById(id))
-    })
+    try {
+      if (await bcrypt.compare(password, user.password)) {
+        return done(null, user)
+      } else {
+        return done(null, false, { message: 'Password incorrect' })
+      }
+    } catch (e) {
+      return done(e)
+    }
+  }
 
-} 
-module.exports=intialatize
+  passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
+  passport.serializeUser((user, done) => done(null, user.id))
+  passport.deserializeUser((id, done) => {
+    return done(null, getUserById(id))
+  })
+}
+
+module.exports = initialize
